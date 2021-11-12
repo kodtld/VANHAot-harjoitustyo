984fe2243cd7317d797dd2038273c37adaeb9059
Konsta <kodu@live.fi>
2021-11-12 04:06:13 +0200

    Kaikki testaukset lisätty ja coverage 100%

--- laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -1,5 +1,6 @@
 import unittest
 from kassapaate import Kassapaate
+from maksukortti import Maksukortti
 
 class TestKassapaate(unittest.TestCase):
     def setUp(self):
@@ -12,9 +13,6 @@ class TestKassapaate(unittest.TestCase):
 
     def test_lounaiden_maara_alussa(self):
         self.assertEqual(int(self.kassa.edulliset + self.kassa.maukkaat),0)
-
-
-
 
 # ----- KÄTEISMAKSUT -----
 
@@ -75,3 +73,119 @@ class TestKassapaate(unittest.TestCase):
         self.assertEqual(int(self.kassa.maukkaat), 0)    
 
 # ----- KORTTIMAKSUT -----
+
+# ----- Edulliset -----
+
+### Riittävä korttimaksu
+
+    def test_edullinen_jos_korttimaksu_riittava_korttia_veloitetaan(self):
+        self.kortti = Maksukortti(400)
+        self.kassa.syo_edullisesti_kortilla(self.kortti)
+        self.assertEqual(int(self.kortti.saldo), 160)
+
+    def test_edullinen_jos_korttimaksu_riittava_true(self):
+        self.kortti = Maksukortti(400)
+        self.assertEqual(self.kassa.syo_edullisesti_kortilla(self.kortti),True)
+
+    def test_edullinen_jos_korttimaksu_riittava_lounaiden_maara_kasvaa(self):
+        self.kortti = Maksukortti(400)
+        self.kassa.syo_edullisesti_kortilla(self.kortti)
+        self.assertEqual(int(self.kassa.edulliset), 1)
+
+    def test_edullinen_jos_korttimaksu_riittava_kassa_ei_kasva(self):
+        self.kortti = Maksukortti(400)
+        self.kassa.syo_edullisesti_kortilla(self.kortti)
+        self.assertEqual(int(self.kassa.kassassa_rahaa), 100000)
+    
+
+### Vajaa korttimaksu
+
+    def test_edullinen_jos_korttimaksu_vajaa_korttia_ei_veloiteta(self):
+        self.kortti = Maksukortti(140)
+        self.kassa.syo_edullisesti_kortilla(self.kortti)
+        self.assertEqual(int(self.kortti.saldo), 140)
+
+    def test_edullinen_jos_korttimaksu_vajaa_false(self):
+        self.kortti = Maksukortti(140)
+        self.assertEqual(self.kassa.syo_edullisesti_kortilla(self.kortti),False)
+
+    def test_edullinen_jos_korttimaksu_vajaa_lounaiden_maara_ei_kasva(self):
+        self.kortti = Maksukortti(140)
+        self.kassa.syo_edullisesti_kortilla(self.kortti)
+        self.assertEqual(int(self.kassa.edulliset), 0)
+
+    def test_edullinen_jos_korttimaksu_vajaa_kassa_ei_kasva(self):
+        self.kortti = Maksukortti(140)
+        self.kassa.syo_edullisesti_kortilla(self.kortti)
+        self.assertEqual(int(self.kassa.kassassa_rahaa), 100000)
+
+
+# ----- Maukkaat -----
+
+### Riittävä korttimaksu
+
+    def test_maukas_jos_korttimaksu_riittava_korttia_veloitetaan(self):
+        self.kortti = Maksukortti(600)
+        self.kassa.syo_maukkaasti_kortilla(self.kortti)
+        self.assertEqual(int(self.kortti.saldo), 200)
+
+    def test_maukas_jos_korttimaksu_riittava_true(self):
+        self.kortti = Maksukortti(600)
+        self.assertEqual(self.kassa.syo_maukkaasti_kortilla(self.kortti),True)
+
+    def test_maukas_jos_korttimaksu_riittava_lounaiden_maara_kasvaa(self):
+        self.kortti = Maksukortti(600)
+        self.kassa.syo_maukkaasti_kortilla(self.kortti)
+        self.assertEqual(int(self.kassa.maukkaat), 1)
+
+    def test_maukas_jos_korttimaksu_riittava_kassa_ei_kasva(self):
+        self.kortti = Maksukortti(600)
+        self.kassa.syo_maukkaasti_kortilla(self.kortti)
+        self.assertEqual(int(self.kassa.kassassa_rahaa), 100000)
+
+### Vajaa korttimaksu
+
+    def test_maukas_jos_korttimaksu_vajaa_korttia_ei_veloiteta(self):
+        self.kortti = Maksukortti(140)
+        self.kassa.syo_maukkaasti_kortilla(self.kortti)
+        self.assertEqual(int(self.kortti.saldo), 140)
+
+    def test_maukas_jos_korttimaksu_vajaa_false(self):
+        self.kortti = Maksukortti(140)
+        self.assertEqual(self.kassa.syo_maukkaasti_kortilla(self.kortti),False)
+
+    def test_maukas_jos_korttimaksu_vajaa_lounaiden_maara_ei_kasva(self):
+        self.kortti = Maksukortti(140)
+        self.kassa.syo_maukkaasti_kortilla(self.kortti)
+        self.assertEqual(int(self.kassa.maukkaat), 0)
+
+    def test_maukas_jos_korttimaksu_vajaa_kassa_ei_kasva(self):
+        self.kortti = Maksukortti(140)
+        self.kassa.syo_maukkaasti_kortilla(self.kortti)
+        self.assertEqual(int(self.kassa.kassassa_rahaa), 100000)
+
+# ----- SALDON LATAAMINEN KORTILLE -----
+
+### Positiivinen saldolataus
+
+    def test_positiivinen_saldolataus_saldo_kasvaa(self):
+        self.kortti = Maksukortti(100)
+        self.kassa.lataa_rahaa_kortille(self.kortti,300)
+        self.assertEqual(int(self.kortti.saldo), 400)
+
+    def test_positiivinen_saldolataus_kassa_kasvaa(self):
+        self.kortti = Maksukortti(100)
+        self.kassa.lataa_rahaa_kortille(self.kortti,300)
+        self.assertEqual(int(self.kassa.kassassa_rahaa), 100300)
+
+### Negatiivinen saldolataus
+
+    def test_negatiivinen_saldolataus_saldo_ei_kasva(self):
+        self.kortti = Maksukortti(100)
+        self.kassa.lataa_rahaa_kortille(self.kortti,-300)
+        self.assertEqual(int(self.kortti.saldo), 100)
+
+    def test_negatiivinen_saldolataus_kassa_ei_kasva(self):
+        self.kortti = Maksukortti(100)
+        self.kassa.lataa_rahaa_kortille(self.kortti,-300)
+        self.assertEqual(int(self.kassa.kassassa_rahaa), 100000)
