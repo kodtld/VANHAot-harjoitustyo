94c445739a1bf5f50e4fb7754676bd36ba6a1cc9
Konsta <kodu@live.fi>
2021-11-19 12:01:40 +0200

    Log_In.py

--- src/Log_In.py
+++ src/Log_In.py
@@ -1,9 +1,6 @@
 from tkinter import *
 import csv
 
-with open("User_Data.csv","r") as ud:
-    reader = csv.reader(ud)
-
 screen = Tk()
 screen.geometry("950x750")
 screen.title("Opintojen seuranta")
@@ -18,11 +15,25 @@ User_password.pack()
 User_password.insert(0,"Password")
 
 
+def Succes():
+    viesti = Label(screen,text="Onnittelut tämä toimii").pack()
+    
+
+def Fail():
+    viesti = Label(screen,text="Ei natsaa").pack()
+    
+
 def Login():
     usna = User_name.get()
     uspa = User_password.get()
     
-
+    with open("User_Data.csv","r") as ud:
+        reader = csv.reader(ud, delimiter=",")
+        
+        if [usna,uspa] in reader:
+            Succes()
+        else:
+            Fail()
 
 Button(text="Login",height="3",width="30",command=Login).pack()
 
@@ -37,8 +48,7 @@ Button(text="Register",height="3",width="30").pack()
 
 
 
-if __name__ == "__main__":
-    screen.mainloop()
+screen.mainloop()
     
 
 
