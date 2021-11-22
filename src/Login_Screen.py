f3d4c711b212c09c871ab72cbee9891c4ad6b267
Konsta <kodu@live.fi>
2021-11-22 11:22:40 +0200

    poetry install tehty ot-harjoitustyö kansioon sekä login.py ja user data lisätty src kansioon

---
+++ src/Login_Screen.py
@@ -0,0 +1,83 @@
+from tkinter import *
+import csv
+
+screen = Tk()
+screen.geometry("950x750")
+screen.title("Opintojen seuranta")
+
+User_name = Entry(screen, width="75")
+User_name.pack()
+User_name.insert(0,"Username")
+
+User_password = Entry(screen,width="75")
+User_password.pack()
+User_password.insert(0,"Password")
+
+def Login():
+    usna = User_name.get()
+    uspa = User_password.get()
+    
+    def Succes():
+        viesti = Label(screen,text="Onnittelut tämä toimii").pack()
+
+    def Fail():
+        viesti = Label(screen,text="Wrong username or password").pack()
+    
+    with open("User_Data.csv","r") as ud:
+        reader = csv.reader(ud, delimiter=",")    
+        if [usna,uspa] in reader:
+
+            # Tarkoituksena on tulevaisuudessa kutsua funktiota joka avaa sovelluksen, Succes on väliaikainen täyte
+            Succes()
+        else:
+            Fail()
+    ud.close()
+
+def Register():
+    usna = User_name.get()
+    uspa = User_password.get()
+
+    Used_names=[]
+    with open("User_Data.csv","r") as ud:
+        reader = csv.reader(ud, delimiter=",")
+        for row in reader:
+            Used_names.append(row[0])
+    ud.close()
+
+    # Inner functions
+    def Succes():
+        viesti = Label(screen,text="Succes! You can now login with your credentials").pack()
+
+    def Fail():
+        viesti = Label(screen,text="That username is already taken").pack()
+
+    def Character_check():
+        viesti = Label(screen,text="Username can't contain spaces, or the following characters: ä, å, ö").pack()
+
+    def Correct_Lenght():
+        viesti = Label(screen,text="Username must be 4-15 characters long").pack()
+
+    if len(usna) < 4 or len(usna) > 15:
+        Correct_Lenght()
+
+    elif usna not in Used_names:
+        if " " in usna or "ä" in usna or "å" in usna or "ö" in usna:
+            Character_check()
+        else:    
+            with open("User_Data.csv","a") as ud:
+                writer = csv.writer(ud,lineterminator='\n')
+                writer.writerow([usna,uspa])
+                ud.close()
+                Succes()
+    
+    else:
+        Fail()
+
+Button(text="Login",height="3",width="30",command=Login).pack()
+
+Label(text="").pack()
+
+Button(text="Register",height="3",width="30",command=Register).pack()
+
+screen.mainloop()
+    
